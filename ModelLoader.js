39124034fcfed62be505aa9f9c34cc651c48c53b
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2023-02-26 17:22:00 +0200

    changing the model size and shape

--- ModelLoader.js
+++ ModelLoader.js
@@ -3,9 +3,9 @@ import {GLTFLoader} from './GLTFLoader.js';
 async function loadmodel(path){
     const loader=new GLTFLoader();
     const modelData =await loader.loadAsync(path);
-    const model = modelData.scene.children[0];
-    model.scale.set(3,3,3);
-    model.position.set(2,.02,1);
+    const model = modelData.scene;
+    //model.scale.set(3,3,3);
+    //model.position.set(2,.02,1);
     return {model};
 }
 export{loadmodel};
