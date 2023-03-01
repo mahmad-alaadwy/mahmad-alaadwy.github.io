af2b2c101a989762adaccdec31159f205ef10a18
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2023-03-01 22:58:04 +0200

    changing the scale

--- ModelLoader.js
+++ ModelLoader.js
@@ -4,7 +4,7 @@ async function loadmodel(path){
     const loader=new GLTFLoader();
     const modelData =await loader.loadAsync(path);
     const model = modelData.scene;
-    //model.scale.set(3,3,3);
+    model.scale.set(.03,.03,.03);
     //model.position.set(2,.02,1);
     return {model};
 }
